c115187e47b9f5554ce4d02e02438e7932e94583
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2021-01-24 14:00:08 +0200

    Implement logout

--- routes/user.js
+++ routes/user.js
@@ -10,6 +10,11 @@ router.get("/register", (req, res) => {
   res.render("../views/user/register");
 });
 
+router.get("/logout", (req, res) => {
+  res.clearCookie("userToken");
+  res.redirect("/");
+});
+
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
   const token = await userController.login(username, password);
