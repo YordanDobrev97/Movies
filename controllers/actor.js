f95d1fcd6d81c531858b1a365a0c52057e96c4b1
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2021-03-28 00:10:57 +0200

    Improved and clean code

--- controllers/actor.js
+++ controllers/actor.js
@@ -1,5 +1,14 @@
 const actorService = require("../services/actorService");
 
+module.exports.getAll = async function () {
+  const all = await actorService.getAll();
+  return all;
+};
+
+module.exports.add = async function (name, image, resultMovies) {
+  await ActorService.add(name, image, resultMovies);
+};
+
 module.exports.getById = async function (id) {
   const actor = await actorService.getById(id);
   return actor;
--- routes/actor.js
+++ routes/actor.js
@@ -2,11 +2,10 @@ const express = require("express");
 const { authentication } = require("../middleware/auth");
 const { isAdmin } = require("../middleware/admin");
 const actorController = require("../controllers/actor");
-const ActorService = require("../services/actorService");
 const router = express.Router();
 
 router.get("/", authentication, isAdmin, async (req, res) => {
-  const actors = await ActorService.getAll();
+  const actors = await actorController.getAll();
   res.render("../views/actors/index", {
     isAuth: req.isAuth,
     isAdmin: req.isAdmin,
@@ -35,7 +34,7 @@ router.get("/add", authentication, isAdmin, (req, res) => {
 router.post("/add", authentication, async (req, res) => {
   let { name, movies, image } = req.body;
   const resultMovies = movies.split(", ");
-  await ActorService.add(name, image, resultMovies);
+  await actorController.add(name, image, resultMovies);
   res.redirect("/actors");
 });
 
